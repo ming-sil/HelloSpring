7797302b69ef8bb4be2d1cfce1a1e2634d620260
choming <choming4926@innerwave.co.kr>
2024-01-16 10:46:31 +0900

    DB 연동

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -2,20 +2,14 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
-@Service
+
 public class MemberService {
 
-    private final MemberRepository memberRepository; //=new MemoryMemberRepository();
-
-    @Autowired
+    private final MemberRepository memberRepository;
     public MemberService(MemberRepository memberRepository){
         this.memberRepository = memberRepository;
     }
@@ -26,7 +20,6 @@ public class MemberService {
     public Long join(Member member){
         // 중복회원 검증
         validateDuplicate(member);
-
 
         memberRepository.save(member);
         return member.getId();
