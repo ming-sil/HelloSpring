732e382eae846f31cae9a92c96c07ee04b48e73c
choming <choming4926@innerwave.co.kr>
2024-01-16 13:15:43 +0900

    JPA 적용

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,11 +1,17 @@
 package hello.hellospring.domain;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+
+@Entity
 @Getter
 @Setter
 public class Member {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "name")
     private String name;
 }
